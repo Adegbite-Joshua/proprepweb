6c4163628594b73b6f5ba88f9e1405928684d17d
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2024-03-08 15:59:23 +0100

    Created web for downloading the proprep apk file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 'use client'
 
-import { Inter } from 'next/font/google'
-import { useEffect, useState } from "react";
+import { Inter } from 'next/font/google';
+import { useEffect, useRef, useState } from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   const [downloaded, setDownloaded] = useState(false);
+  const isMountedRef = useRef(true);
+
   const handleDownload = () => {
     const link = document.createElement('a');
     link.href = '/proprep.apk';
@@ -16,12 +18,22 @@ export default function Home() {
     document.body.removeChild(link);
     setDownloaded(true);
   };
+
   useEffect(() => {
-    handleDownload();
-  }, [])
+    isMountedRef.current = true;
+
+    if (isMountedRef.current) {
+      handleDownload();
+    }
+
+    return () => {
+      isMountedRef.current = false;
+    };
+  }, []);
+
   return (
     <div>
-      {downloaded && (<p>Thanks for downloading</p>)}
+      {downloaded && <p>Thanks for downloading</p>}
     </div>
-  )
+  );
 }
