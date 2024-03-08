f9b38848585b5bfd45fd6809ed8919a56b980f2e
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2024-03-08 16:25:33 +0100

    Created web for downloading the proprep apk file

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,35 +6,35 @@ import { useEffect, useRef, useState } from 'react';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  // const [downloaded, setDownloaded] = useState(false);
-  // const isMountedRef = useRef(true);
+  const [downloaded, setDownloaded] = useState(false);
+  const isMountedRef = useRef(true);
 
-  // const handleDownload = () => {
-  //   const link = document.createElement('a');
-  //   link.href = '/proprep.apk';
-  //   link.download = 'proprep.apk';
-  //   document.body.appendChild(link);
-  //   link.click();
-  //   document.body.removeChild(link);
-  //   setDownloaded(true);
-  // };
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = '/proprep.apk';
+    link.download = 'proprep.apk';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    setDownloaded(true);
+  };
 
-  // useEffect(() => {
-  //   isMountedRef.current = true;
+  useEffect(() => {
+    isMountedRef.current = true;
 
-  //   if (isMountedRef.current) {
-  //     handleDownload();
-  //   }
+    if (isMountedRef.current) {
+      handleDownload();
+    }
 
-  //   return () => {
-  //     isMountedRef.current = false;
-  //   };
-  // }, []);
+    return () => {
+      isMountedRef.current = false;
+    };
+  }, []);
 
   return (
     <div>
     <p>PROPREP</p>
-      {/* {downloaded && <p>Thanks for downloading</p>} */}
+      {downloaded && <p>Thanks for downloading</p>}
     </div>
   );
 }
